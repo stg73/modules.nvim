97242669c389a533c58a2071fc7984519e6594dc
STG <stg737467@gmail.com>
2025-11-15 13:12:29 +0900

    improve(open_webpage): access()がvim.system()の返値を返すように

--- lua/open_webpage.lua
+++ lua/open_webpage.lua
@@ -4,20 +4,21 @@ local str = require("string_utils")
 
 function M.access(url)
     local temp = vim.fn.tempname()
-    local job = vim.system({
+    return vim.system({
         "curl",
         "-L",
         url,
         "-o",
         temp
-    },{}):wait()
-    if job.code == 0 then
-        vim.cmd.edit(temp)
-        vim.b.URL = url -- URLが後から分かるように
-    else
-        vim.fn.feedkeys(":","nx") -- エラー出力の上部に前のメッセージが表示されぬよう
-        error("\n> curl -L " .. url .. " -o " .. temp .. "\n" .. job.stderr)
-    end
+    },vim.schedule_wrap(function(job)
+        if job.code == 0 then
+            vim.cmd.edit(temp)
+            vim.b.URL = url -- URLが後から分かるように
+        else
+            vim.fn.feedkeys(":","nx") -- エラー出力の上部に前のメッセージが表示されぬよう
+            error("\n> curl -L " .. url .. " -o " .. temp .. "\n" .. job.stderr)
+        end
+    end)):wait()
 end
 
 function M.open(url)
