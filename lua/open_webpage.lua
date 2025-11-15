9242ddf32d2109ede217ea8d5bd780c96a13166c
STG <stg737467@gmail.com>
2025-11-15 13:28:55 +0900

    refactor(open_webpage): 読みやすく変数に分ける

--- lua/open_webpage.lua
+++ lua/open_webpage.lua
@@ -2,6 +2,12 @@ local M = {}
 
 local str = require("string_utils")
 
+local function set_filetype(url)
+    if vim.o.filetype == "" then
+        vim.bo.filetype = vim.filetype.match({ filename = str.get.path_of_url(url) or "", buf = 0 }) or "html"
+    end
+end
+
 function M.open(url)
     local temp = vim.fn.tempname()
     return vim.system({
@@ -13,13 +19,13 @@ function M.open(url)
     },vim.schedule_wrap(function(job)
         if job.code == 0 then
             vim.cmd.edit(temp)
-            if vim.o.filetype == "" then
-                vim.bo.filetype = vim.filetype.match({ filename = str.get.path_of_url(url) or "", buf = 0 }) or "html"
-            end
+            set_filetype(url)
             vim.b.URL = url -- URLが後から分かるように
         else
             vim.fn.feedkeys(":","nx") -- エラー出力の上部に前のメッセージが表示されぬよう
-            error("\n> curl -L " .. url .. " -o " .. temp .. "\n" .. job.stderr)
+            local prompt = ">"
+            local command = "curl -L " .. url .. " -o " .. temp
+            error("\n" .. prompt .. " " .. command .. "\n" .. job.stderr)
         end
     end)):wait()
 end
