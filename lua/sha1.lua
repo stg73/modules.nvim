90db2dfd5e561a8edcc5d54d058e4903325ab4f7
STG <stg737467@gmail.com>
2026-02-04 11:16:54 +0900

    fix(sha1): 正しく実装できていなかったので修正

--- lua/sha1.lua
+++ lua/sha1.lua
@@ -9,8 +9,12 @@ function M.sha1(msg)
 
     -- パディング
     table.insert(bytes,0x80)
-    local number_of_0 = 64 - (((#bytes + 8 - 1) % 64) + 1)
-    vim.list_extend(bytes,tbl.map(function() return 0x00 end)(tbl.range(1)(number_of_0)))
+    local non_zero_bytes = #bytes + 8
+    local current_mod = non_zero_bytes % 64
+    if current_mod ~= 0 then
+        local second_append = tbl.map(function() return 0x00 end)(tbl.range(1)(64 - current_mod))
+        vim.list_extend(bytes,second_append)
+    end
     local msg_bit_len = string.len(msg) * 8
     vim.list_extend(bytes,base.align(8)(base.to(2 ^ 8)(msg_bit_len)))
 
@@ -20,62 +24,66 @@ function M.sha1(msg)
     local H3 = 0x10325476
     local H4 = 0xC3D2E1F0
 
-    local function S(n,x)
-        return bit.bor(bit.lshift(x,n),bit.rshift(x,32 - n))
-    end
+    local function update_state(block)
+        local A = H0
+        local B = H1
+        local C = H2
+        local D = H3
+        local E = H4
 
-    local function f(t,B,C,D)
-        if 0 < t and t <= 20 then
-            return bit.bor(bit.band(B,C),bit.band(bit.bnot(B),D))
-        elseif 20 < t and t <= 40 then
-            return bit.bxor(B,C,D)
-        elseif 40 < t and t <= 60 then
-            return bit.bor(bit.band(B,C),bit.band(B,D),bit.band(C,D))
-        elseif 60 < t and t <= 80 then
-            return bit.bxor(B,C,D)
+        local function S(n,x)
+            return bit.bor(bit.lshift(x,n),bit.rshift(x,32 - n))
         end
-    end
 
-    local function K(t)
-        if 0 < t and t <= 20 then
-            return 0x5A827999
-        elseif 20 < t and t <= 40 then
-            return 0x6ED9EBA1
-        elseif 40 < t and t <= 60 then
-            return 0x8F1BBCDC
-        elseif 60 < t and t <= 80 then
-            return 0xCA62C1D6
+        local function f(t,B,C,D)
+            if t <= 20 then
+                return bit.bor(bit.band(B,C),bit.band(bit.bnot(B),D))
+            elseif t <= 40 then
+                return bit.bxor(B,C,D)
+            elseif t <= 60 then
+                return bit.bor(bit.band(B,C),bit.band(B,D),bit.band(C,D))
+            elseif t <= 80 then
+                return bit.bxor(B,C,D)
+            end
         end
+
+        local function K(t)
+            if t <= 20 then
+                return 0x5A827999
+            elseif t <= 40 then
+                return 0x6ED9EBA1
+            elseif t <= 60 then
+                return 0x8F1BBCDC
+            elseif t <= 80 then
+                return 0xCA62C1D6
+            end
+        end
+
+        local W = {}
+        tbl.map(function(list)
+            table.insert(W,base.from(2 ^ 8)(list))
+        end)(tbl.chunks(4)(block))
+        tbl.map(function(i)
+            W[i] = S(1,bit.bxor(W[i - 3],W[i - 8],W[i - 14],W[i - 16]))
+        end)(tbl.range(17)(80))
+
+        tbl.map(function(t)
+            local TEMP = S(5,A) + f(t,B,C,D) + E + W[t] + K(t)
+            E = D
+            D = C
+            C = S(30,B)
+            B = A
+            A = TEMP
+        end)(tbl.range(1)(80))
+
+        H0 = (H0 + A) % (2 ^ 32)
+        H1 = (H1 + B) % (2 ^ 32)
+        H2 = (H2 + C) % (2 ^ 32)
+        H3 = (H3 + D) % (2 ^ 32)
+        H4 = (H4 + E) % (2 ^ 32)
     end
 
-    local W = {}
-    tbl.map(function(list)
-        table.insert(W,base.from(2 ^ 8)(list))
-    end)(tbl.chunks(4)(bytes))
-    tbl.map(function(i)
-        W[i] = S(1,bit.bxor(W[i - 3],W[i - 8],W[i - 14],W[i - 16]))
-    end)(tbl.range(17)(80))
-
-    local A = H0
-    local B = H1
-    local C = H2
-    local D = H3
-    local E = H4
-
-    tbl.map(function(t)
-        local TEMP = S(5,A) + f(t,B,C,D) + E + W[t] + K(t)
-        E = D
-        D = C
-        C = S(30,B)
-        B = A
-        A = TEMP
-    end)(tbl.range(1)(80))
-
-    H0 = (H0 + A) % (2 ^ 32)
-    H1 = (H1 + B) % (2 ^ 32)
-    H2 = (H2 + C) % (2 ^ 32)
-    H3 = (H3 + D) % (2 ^ 32)
-    H4 = (H4 + E) % (2 ^ 32)
+    tbl.map(update_state)(tbl.chunks(64)(bytes))
 
     return table.concat(tbl.map(function(H)
         local big_endian = base.to(2 ^ 8)(H)
