3d7185fd89b702e1ae895ea08c0aee344899f1f7
STG <stg737467@gmail.com>
2026-03-02 12:52:18 +0900

    refactor(sha1): 関数型っぽく、状態の更新に fold() を使う

--- lua/sha1.lua
+++ lua/sha1.lua
@@ -4,7 +4,87 @@ local base = require("convert_base")
 local tbl = require("tbl")
 
 -- 参考にした記事: https://qiita.com/mikecat_mixc/items/8e3ca5f323cfda848220
+
+local function S(n,x)
+    return bit.bor(bit.lshift(x,n),bit.rshift(x,32 - n))
+end
+
+local function f(t,B,C,D)
+    if t <= 20 then
+        return bit.bor(bit.band(B,C),bit.band(bit.bnot(B),D))
+    elseif t <= 40 then
+        return bit.bxor(B,C,D)
+    elseif t <= 60 then
+        return bit.bor(bit.band(B,C),bit.band(B,D),bit.band(C,D))
+    elseif t <= 80 then
+        return bit.bxor(B,C,D)
+    end
+end
+
+local function K(t)
+    if t <= 20 then
+        return 0x5A827999
+    elseif t <= 40 then
+        return 0x6ED9EBA1
+    elseif t <= 60 then
+        return 0x8F1BBCDC
+    elseif t <= 80 then
+        return 0xCA62C1D6
+    end
+end
+
+local function update_state(H,block)
+    local W = {}
+    tbl.map(function(list)
+        table.insert(W,base.from(2 ^ 8)(list))
+    end)(tbl.chunks(4)(block))
+    tbl.map(function(i)
+        W[i] = S(1,bit.bxor(W[i - 3],W[i - 8],W[i - 14],W[i - 16]))
+    end)(tbl.range(17)(80))
+
+    local I = tbl.fold(function(H,t)
+        return {
+            S(5,H[1]) + f(t,H[2],H[3],H[4]) + H[5] + W[t] + K(t),
+            H[1],
+            S(30,H[2]),
+            H[3],
+            H[4],
+        }
+    end)(H)(tbl.range(1)(80))
+
+    return tbl.map(function(t)
+        return (t[1] + t[2]) % (2 ^ 32)
+    end)(tbl.zip({H,I}))
+end
+
+local initial_H = { 0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0 }
+
 function M.sha1(msg)
+    local bytes = { string.byte(msg,1,-1) }
+
+    -- パディング
+    table.insert(bytes,0x80)
+    local non_zero_bytes = #bytes + 8
+    local current_mod = non_zero_bytes % 64
+    if current_mod ~= 0 then
+        local second_append = tbl.map(tbl.const(0x00))(tbl.range(1)(64 - current_mod))
+        vim.list_extend(bytes,second_append)
+    end
+    local msg_bit_len = string.len(msg) * 8
+    vim.list_extend(bytes,base.align(8)(base.to(2 ^ 8)(msg_bit_len)))
+
+    local final_H = tbl.fold(update_state)(initial_H)(tbl.chunks(64)(bytes))
+
+    return tbl.pipe {
+        final_H,
+        tbl.map(base.to(2 ^ 8)),
+        tbl.flatten(),
+        tbl.map(string.char),
+        table.concat,
+    }
+end
+
+function M.sha1_(msg)
     local bytes = { string.byte(msg,1,-1) }
 
     -- パディング
