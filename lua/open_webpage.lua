f1eecdba06097ebc2e1f656cd980ba0acdfce225
STG <stg737467@gmail.com>
2026-05-10 13:31:45 +0900

    fix(open_webpage): filetypeの検出方法を改善

--- lua/open_webpage.lua
+++ lua/open_webpage.lua
@@ -2,12 +2,6 @@ local M = {}
 
 local str = require("string_utils")
 
-local function set_filetype(url)
-    if vim.o.filetype == "" then
-        vim.bo.filetype = vim.filetype.match({ filename = str.get.path_of_url(url) or "", buf = 0 }) or "html"
-    end
-end
-
 function M.open(url)
     local temp = vim.fn.tempname()
     return vim.system({
@@ -19,13 +13,15 @@ function M.open(url)
     },vim.schedule_wrap(function(job)
         if job.code == 0 then
             vim.cmd.edit(temp)
-            set_filetype(url)
+            vim.api.nvim_buf_set_name(0,url)
+            vim.api.nvim_exec_autocmds("BufRead",{ group = "filetypedetect", buf = 0 })
+            vim.api.nvim_buf_set_name(0,temp)
             vim.b.URL = url -- URLが後から分かるように
         else
             vim.fn.feedkeys(":","nx") -- エラー出力の上部に前のメッセージが表示されぬよう
             local prompt = ">"
-            local command = "curl -L " .. url .. " -o " .. temp
-            error("\n" .. prompt .. " " .. command .. "\n" .. job.stderr)
+            local command = ("curl -L %s -o %s"):format(url,temp)
+            vim.notify(("\n%s %s\n%s"):format(prompt,command,job.stderr),vim.log.levels.ERROR)
         end
     end)):wait()
 end
